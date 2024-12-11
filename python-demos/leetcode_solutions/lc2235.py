74189b284176aa8003eb418f75c0744ab6bf160e
Peter Song <peter.2.song@outlook.com>
2024-12-11 22:49:05 +0800

    leet code 2235

---
+++ python-demos/leetcode_solutions/lc2235.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/add-two-integers/
+# 2235. Add Two Integers
+
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+
+
+def main():
+    num1 = 2
+    num2 = 8
+    expected_result = 10
+    r = Solution().sum(num1, num2) == expected_result
+    print(f"Is passed: {r}")
+    assert r
+
+
+if __name__ == "__main__":
+    main()
