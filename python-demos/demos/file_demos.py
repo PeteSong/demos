a2c5bef89eda25235159dc88e9ac642077f4f7ed
Peter Song <peter.2.song@outlook.com>
2025-02-14 21:13:27 +0800

    fsplit2 by size

--- python-demos/demos/file_demos.py
+++ python-demos/demos/file_demos.py
@@ -16,6 +16,8 @@ from typing import Optional
 
 
 def fsplit(src_file_path: str, line_num: int = 1000, target_file_prefix: Optional[str] = None) -> None:
+    """split file by line number"""
+
     def _write_split_file() -> None:
         target_file_path = f"{target_file_prefix}_{split_count}"
         # with newline="", the line terminators are written without translation.
@@ -30,7 +32,7 @@ def fsplit(src_file_path: str, line_num: int = 1000, target_file_prefix: Optiona
     if target_file_prefix is None:
         target_file_prefix = os.path.basename(src_file_path)
     # with newline="", the line terminators are written without translation.
-    with open(src_file_path, encoding="utf-8", newline="") as src_file:
+    with open(src_file_path, "r", encoding="utf-8", newline="") as src_file:
         line_count = 0
         # with newline="", the line terminators are written without translation.
         lines = io.StringIO(newline="")
@@ -42,9 +44,27 @@ def fsplit(src_file_path: str, line_num: int = 1000, target_file_prefix: Optiona
                 _write_split_file()
                 split_count += 1
                 line_count = 0
-                lines = io.StringIO()
+                lines = io.StringIO(newline="")
         if line_count > 0:
             _write_split_file()
+
+
+def fsplit2(src_file_path: str, size: int = 1024 * 1024, target_file_prefix: Optional[str] = None) -> None:
+    """split file by size(bytes)"""
+    src_file_path = os.path.expanduser(src_file_path)
+    if not os.path.isfile(src_file_path):
+        raise ValueError(f"File not found or not a regular file: {src_file_path}")
+    if target_file_prefix is None:
+        target_file_prefix = os.path.basename(src_file_path)
+    # with newline="", the line terminators are written without translation.
+    with open(src_file_path, "rb") as src_file:
+        split_count = 0
+        while trunk := src_file.read(size):
+            target_file_path = f"{target_file_prefix}_b{split_count}"
+            with open(target_file_path, "wb") as target_file:
+                target_file.write(trunk)
+            print(f"{target_file_path} created.")
+            split_count += 1
 
 
 def fmerge(src_file_paths: list[str], target_file_path: str = "target_merged_file") -> None:
@@ -56,11 +76,9 @@ def fmerge(src_file_paths: list[str], target_file_path: str = "target_merged_fil
         if not p.is_file():
             raise ValueError(f"File not found or not a regular file: {src_file_path}")
         paths.append(p)
-    # with newline="", the line terminators are written without translation.
-    with open(target_file_path, "w", encoding="utf-8", newline="") as target_file:
+    with open(target_file_path, "wb") as target_file:
         for p in paths:
-            # with newline="", the line terminators are written without translation.
-            with open(p, encoding="utf-8", newline="") as src_file:
+            with open(p, "rb") as src_file:
                 target_file.write(src_file.read())
     print(f"{target_file_path} created.")
 
@@ -86,9 +104,12 @@ def fmd5(src_file_path: str) -> str:
 def main() -> None:  # pragma: no cover
     fsplit("./data/pg26184.txt", 500)
     fmerge(["./pg26184.txt_0", "./pg26184.txt_1", "./pg26184.txt_2", "./pg26184.txt_3"], "pg26184.txt_merged")
-    print(fmd5("./data/pg26184.txt"))
+    print(fmd5("./data/pg26184.txt"), fmd5("pg26184.txt_merged"))
+
+    fsplit2("./data/pg26184.txt", 1024 * 20)
+    fmerge(["./pg26184.txt_b0", "./pg26184.txt_b1", "./pg26184.txt_b2", "./pg26184.txt_b3"], "pg26184.txt_bmerged")
+    print(fmd5("./data/pg26184.txt"), fmd5("pg26184.txt_bmerged"))
 
 
 if __name__ == "__main__":
-    # main()
-    print(fmd5("./data/pg26184.txt"))
+    main()
--- python-demos/tests/demos/test_file_demos.py
+++ python-demos/tests/demos/test_file_demos.py
@@ -1,3 +1,5 @@
+import filecmp
+
 import pytest
 
 import demos.file_demos as fds
@@ -9,6 +11,8 @@ invalid_file_paths = ["", "/not/exist/file"]
 def test_fsplit_with_invalid_file(file_path):
     with pytest.raises(ValueError):
         fds.fsplit(file_path)
+    with pytest.raises(ValueError):
+        fds.fsplit2(file_path)
 
 
 invalid_src_file_paths = [[], ["/only/one/file"], ["/not/exist/file1", "/not/exist/file2"]]
@@ -23,12 +27,49 @@ def test_fmerge_with_invalid_src_files(src_file_paths):
 def test_fsplit_and_fmerge():
     fds.fsplit("./data/pg26184.txt", 500)
     fds.fmerge(["./pg26184.txt_0", "./pg26184.txt_1", "./pg26184.txt_2", "./pg26184.txt_3"], "pg26184.txt_merged")
-    import filecmp
 
     assert filecmp.cmp("./data/pg26184.txt", "pg26184.txt_merged") is True
     fds.fsplit("./data/pg26184_1.txt", 400, "data.txt")
     fds.fmerge(["./data.txt_0", "./data.txt_1", "./data.txt_2", "./data.txt_3"], "data.txt_merged")
     assert filecmp.cmp("./data/pg26184_1.txt", "data.txt_merged") is True
+
+
+def test_fsplit_and_fmerge_with_tmp_path(tmp_path):
+    d = tmp_path / "test_fsplit_and_fmerge_with_tmp_path"
+    d.mkdir()
+    target_file_prefix = d / "test_data.txt"
+    src_file_path = "./data/pg26184.txt"
+    fds.fsplit(src_file_path, 500, target_file_prefix=target_file_prefix)
+
+    splited_file_paths = sorted([str(d / child) for child in d.iterdir()])
+    target_file_path = str(d / "test_data.txt_merged")
+    fds.fmerge(splited_file_paths, target_file_path)
+
+    assert filecmp.cmp(src_file_path, target_file_path) is True
+
+
+def test_fsplit2_and_fmerge_with_tmp_path(tmp_path):
+    d = tmp_path / "test_fsplit2_and_fmerge_with_tmp_path"
+    d.mkdir()
+    target_file_prefix = d / "test_data.txt"
+    src_file_path = "./data/pg26184.txt"
+    fds.fsplit2(src_file_path, 1024 * 10, target_file_prefix=target_file_prefix)
+
+    splited_file_paths = sorted([str(d / child) for child in d.iterdir()])
+    target_file_path = str(d / "test_data.txt_merged")
+    fds.fmerge(splited_file_paths, target_file_path)
+
+    assert filecmp.cmp(src_file_path, target_file_path) is True
+
+
+def test_fsplit2_and_fmerge():
+    src_file_path = "./data/pg26184.txt"
+    fds.fsplit2(src_file_path, 1024 * 20)
+
+    splited_file_paths = ["pg26184.txt_b0", "pg26184.txt_b1", "pg26184.txt_b2", "pg26184.txt_b3"]
+    fds.fmerge(splited_file_paths)
+
+    assert filecmp.cmp(src_file_path, "target_merged_file") is True
 
 
 @pytest.mark.parametrize("file_path", invalid_file_paths)
