8f7eab0c7fe938141cf768d8afc3638d9aae29c1
Peter Song <peter.2.song@outlook.com>
2025-03-14 15:21:18 +0800

    use @functools.lrc_cache; add Fib generator; refactor;

--- python-demos/algorithms_learning/puzzles.py
+++ python-demos/algorithms_learning/puzzles.py
@@ -1,27 +1,67 @@
+import functools
+
 from demos.func_demo import trace
 
 
 @trace
-def fibonacci(n):
-    """return the n-th Fibonacci number"""
-    if n in [0, 1]:
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+@functools.lru_cache
+def fibonacci_in_recursion(nth):
+    """return the n-th(from 1st) Fibonacci number
+    F(1) = 1, F(2) = 1
+    F(n) = F(n-1) + F(n-2)
+    """
+    if nth <= 0:
+        raise ValueError("n must be greater than 0")
+    if nth in [1, 2]:
+        return 1
+    return fibonacci_in_recursion(nth - 1) + fibonacci_in_recursion(nth - 2)
+
+
+@trace
+def fibonacci(nth):
+    """return the n-th(from 1st) Fibonacci number
+    F(1) = 1, F(2) = 1
+    F(n) = F(n-1) + F(n-2)
+    """
+    if nth <= 0:
+        raise ValueError("n must be greater than 0")
+    n1, n2 = 1, 1
+    for _ in range(nth - 1):
+        n1, n2 = n2, n1 + n2
+    return n1
+
+
+@trace
+def fibonacci_generator(stop_n=None):
+    """return the n-th(from 1st) Fibonacci number
+    F(1) = 1, F(2) = 1
+    F(n) = F(n-1) + F(n-2)
+    """
+    if stop_n is not None and stop_n <= 0:
+        raise ValueError("stop n must be greater than 0")
+    n1, n2 = 1, 1
+    i = 0
+    while True:
+        if stop_n is not None:
+            if i >= stop_n:
+                break
+            else:
+                i += 1
+        yield n1
+        n1, n2 = n2, n1 + n2
 
 
 @trace
 def fibonacci_list(n):
     """return the first n Fibonacci numbers"""
-    if n < 0:
-        raise ValueError("n must be non-negative")
-    if n == 0:
-        return []
+    if n <= 0:
+        raise ValueError("n must be greater than 0")
     if n == 1:
-        return [0]
+        return [1]
     fib_list = [0, 1]
-    for i in range(2, n):
+    for i in range(2, n + 1):
         fib_list.append(fib_list[i - 1] + fib_list[i - 2])
-    return fib_list
+    return fib_list[1:]
 
 
 def main() -> None:  # pragma: no cover
@@ -30,9 +70,16 @@ def main() -> None:  # pragma: no cover
     # print(fibonacci(2))
     # print(fibonacci(3))
     # print(fibonacci(4))
-    # print(fibonacci(5))
+    print(fibonacci(5))
+    print(fibonacci_in_recursion(5))
 
     print(fibonacci_list(5))
+
+    for v in fibonacci_generator(5):
+        print(f"{v}", end=" ")
+    print(list(fibonacci_generator(5)))
+    # for i, v in enumerate(fibonacci_generator(5)):
+    # print(f'{i}: {v}', end=' ')
 
 
 if __name__ == "__main__":
--- python-demos/tests/algorithms_learning/test_puzzles.py
+++ python-demos/tests/algorithms_learning/test_puzzles.py
@@ -1,20 +1,58 @@
 import pytest
 
-from algorithms_learning.puzzles import fibonacci, fibonacci_list
+from algorithms_learning.puzzles import fibonacci, fibonacci_generator, fibonacci_in_recursion, fibonacci_list
 
 
-@pytest.mark.parametrize("n, expected", [(0, 0), (1, 1), (2, 1), (3, 2), (4, 3), (5, 5)])
+@pytest.mark.parametrize("n, expected", [(1, 1), (2, 1), (3, 2), (4, 3), (5, 5)])
 def test_fibonacci(n, expected):
     assert fibonacci(n) == expected
 
 
+@pytest.mark.parametrize("n, expected", [(1, 1), (2, 1), (3, 2), (4, 3), (5, 5)])
+def test_fibonacci_in_recursion(n, expected):
+    assert fibonacci_in_recursion(n) == expected
+
+
 @pytest.mark.parametrize(
-    "n, expected", [(0, []), (1, [0]), (2, [0, 1]), (3, [0, 1, 1]), (4, [0, 1, 1, 2]), (5, [0, 1, 1, 2, 3])]
+    "n, expected",
+    [(1, [1]), (2, [1, 1]), (3, [1, 1, 2]), (4, [1, 1, 2, 3]), (5, [1, 1, 2, 3, 5])],
 )
 def test_fibonacci_list(n, expected):
     assert fibonacci_list(n) == expected
 
 
+@pytest.mark.parametrize(
+    "n, expected",
+    [(1, [1]), (2, [1, 1]), (3, [1, 1, 2]), (4, [1, 1, 2, 3]), (5, [1, 1, 2, 3, 5])],
+)
+def test_fibonacci_generator_with_stop_n(n, expected):
+    assert list(fibonacci_generator(5)) == [1, 1, 2, 3, 5]
+
+
+def test_fibonacci_generator():
+    gen = fibonacci_generator()
+    assert next(gen) == 1
+    assert next(gen) == 1
+    assert next(gen) == 2
+    assert next(gen) == 3
+    assert next(gen) == 5
+    gen.close()
+
+
 def test_fibonacci_list_negative():
     with pytest.raises(ValueError):
         fibonacci_list(-1)
+    with pytest.raises(ValueError):
+        fibonacci_list(0)
+    with pytest.raises(ValueError):
+        fibonacci(-1)
+    with pytest.raises(ValueError):
+        fibonacci(0)
+    with pytest.raises(ValueError):
+        fibonacci_in_recursion(-1)
+    with pytest.raises(ValueError):
+        fibonacci_in_recursion(0)
+    with pytest.raises(ValueError):
+        next(fibonacci_generator(-1))
+    with pytest.raises(ValueError):
+        next(fibonacci_generator(0))
