62412386dd2dc0bedab0a976dce8d2586d5d5373
Peter Song <peter.2.song@outlook.com>
2025-03-14 15:22:00 +0800

    format document; remove the temp files

--- python-demos/tests/demos/test_file_demos.py
+++ python-demos/tests/demos/test_file_demos.py
@@ -1,4 +1,5 @@
 import filecmp
+import os
 
 import pytest
 
@@ -15,7 +16,11 @@ def test_fsplit_with_invalid_file(file_path):
         fds.fsplit_by_byte_size(file_path)
 
 
-invalid_src_file_paths = [[], ["/only/one/file"], ["/not/exist/file1", "/not/exist/file2"]]
+invalid_src_file_paths = [
+    [],
+    ["/only/one/file"],
+    ["/not/exist/file1", "/not/exist/file2"],
+]
 
 
 @pytest.mark.parametrize("src_file_paths", invalid_src_file_paths)
@@ -26,12 +31,34 @@ def test_fmerge_with_invalid_src_files(src_file_paths):
 
 def test_fsplit_and_fmerge():
     fds.fsplit_by_line_number("./data/pg26184.txt", 500)
-    fds.fmerge(["./pg26184.txt_0", "./pg26184.txt_1", "./pg26184.txt_2", "./pg26184.txt_3"], "pg26184.txt_merged")
-
+    fds.fmerge(
+        ["./pg26184.txt_0", "./pg26184.txt_1", "./pg26184.txt_2", "./pg26184.txt_3"],
+        "pg26184.txt_merged",
+    )
     assert filecmp.cmp("./data/pg26184.txt", "pg26184.txt_merged") is True
+    for p in [
+        "./pg26184.txt_0",
+        "./pg26184.txt_1",
+        "./pg26184.txt_2",
+        "./pg26184.txt_3",
+        "pg26184.txt_merged",
+    ]:
+        os.remove(p)
+
     fds.fsplit_by_line_number("./data/pg26184_1.txt", 400, "data.txt")
-    fds.fmerge(["./data.txt_0", "./data.txt_1", "./data.txt_2", "./data.txt_3"], "data.txt_merged")
+    fds.fmerge(
+        ["./data.txt_0", "./data.txt_1", "./data.txt_2", "./data.txt_3"],
+        "data.txt_merged",
+    )
     assert filecmp.cmp("./data/pg26184_1.txt", "data.txt_merged") is True
+    for p in [
+        "./data.txt_0",
+        "./data.txt_1",
+        "./data.txt_2",
+        "./data.txt_3",
+        "data.txt_merged",
+    ]:
+        os.remove(p)
 
 
 def test_fsplit_and_fmerge_with_tmp_path(tmp_path):
@@ -66,10 +93,23 @@ def test_fsplit2_and_fmerge():
     src_file_path = "./data/pg26184.txt"
     fds.fsplit_by_byte_size(src_file_path, 1024 * 20)
 
-    splited_file_paths = ["pg26184.txt_b0", "pg26184.txt_b1", "pg26184.txt_b2", "pg26184.txt_b3"]
+    splited_file_paths = [
+        "pg26184.txt_b0",
+        "pg26184.txt_b1",
+        "pg26184.txt_b2",
+        "pg26184.txt_b3",
+    ]
     fds.fmerge(splited_file_paths)
 
     assert filecmp.cmp(src_file_path, "target_merged_file") is True
+    for p in [
+        "pg26184.txt_b0",
+        "pg26184.txt_b1",
+        "pg26184.txt_b2",
+        "pg26184.txt_b3",
+        "target_merged_file",
+    ]:
+        os.remove(p)
 
 
 @pytest.mark.parametrize("file_path", invalid_file_paths)
