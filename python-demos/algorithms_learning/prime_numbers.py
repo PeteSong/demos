3ee048ed9e3fac760e9b9039bf7cbb63d38a7d08
Peter Song <peter.2.song@outlook.com>
2025-03-20 17:42:11 +0800

    refactor; add method "prime_numbers_in_paralle"(do not use it, it's slow)

--- python-demos/algorithms_learning/prime_numbers.py
+++ python-demos/algorithms_learning/prime_numbers.py
@@ -1,36 +1,115 @@
+import concurrent.futures as cf
+import functools
 import math
+from collections.abc import Iterator
 
 from demos.func_demo import ptimeit
 
+_SMALL_PRIME_NUMBERS = [
+    2,
+    3,
+    5,
+    7,
+    11,
+    13,
+    17,
+    19,
+    23,
+    29,
+    31,
+    37,
+    41,
+    43,
+    47,
+    53,
+    59,
+    61,
+    67,
+    71,
+    73,
+    79,
+    83,
+    89,
+    97,
+]
+_STARTING_NUMBER = _SMALL_PRIME_NUMBERS[-1] + 2
 
+
+def _small_primes(n: int) -> list[int]:
+    if n <= 1:
+        raise ValueError("n must be greater than 1")
+
+    return [x for x in _SMALL_PRIME_NUMBERS if x <= n]
+
+
+@functools.lru_cache
 def is_prime(n: int) -> bool:
     if n <= 1:
-        return False
-    if n <= 3:
+        raise ValueError("n must be greater than 1")
+    if n in _SMALL_PRIME_NUMBERS:
         return True
-    if n % 2 == 0:
+    if any(n % x == 0 for x in _SMALL_PRIME_NUMBERS):
         return False
     k = int(math.floor(math.sqrt(n))) + 1
-    for x in range(3, k, 2):
+    for x in range(_STARTING_NUMBER, k, 2):
         if n % x == 0:
             return False
     return True
 
 
-@ptimeit
-def prime_numbers(n: int) -> list[int]:
+def prime_generator(n: int) -> Iterator[int]:
     if n <= 1:
         raise ValueError("n must be greater than 1")
-    primes = []
-    for i in range(2, n + 1):
+    yield from _small_primes(n)
+    for i in range(_STARTING_NUMBER, n + 1, 2):
         if is_prime(i):
-            primes.append(i)
-    return primes
+            yield i
+
+
+@ptimeit
+def prime_numbers(n: int) -> list[int]:
+    prime_nums = _small_primes(n)
+    for i in range(_STARTING_NUMBER, n + 1, 2):
+        if is_prime(i):
+            prime_nums.append(i)
+    return prime_nums
+
+
+@ptimeit
+def prime_numbers_in_parallel(n: int) -> list[int]:
+    if n <= 1:
+        raise ValueError("n must be greater than 1")
+
+    prime_nums = _small_primes(n)
+
+    futures = {}
+    with cf.ProcessPoolExecutor(10) as executor:
+        for i in range(_STARTING_NUMBER, n + 1, 2):
+            future = executor.submit(is_prime, i)
+            futures[future] = i
+    for future in cf.as_completed(futures):
+        if future.result():
+            prime_nums.append(futures[future])
+    return prime_nums
 
 
 def main() -> None:  # pragma: no cover
-    print(prime_numbers(100))
-    print(prime_numbers(1000))
+    prime_numbers(100)
+    prime_numbers(1000)
+    prime_numbers(10000)
+    # prime_numbers(10 ** 6)
+    # prime_numbers_in_parallel(10 ** 6)
+    g = prime_generator(1)
+    next(g)
+    g = prime_generator(100)
+    print(next(g))
+    print(next(g))
+    print(next(g))
+    print(next(g))
+    print(next(g))
+    print(next(g))
+    for i in g:
+        print(i)
 
 
 if __name__ == "__main__":
--- python-demos/tests/algorithms_learning/test_prime_numbers.py
+++ python-demos/tests/algorithms_learning/test_prime_numbers.py
@@ -1,6 +1,6 @@
 import pytest
 
-from algorithms_learning.prime_numbers import is_prime, prime_numbers
+from algorithms_learning.prime_numbers import is_prime, prime_generator, prime_numbers, prime_numbers_in_parallel
 
 
 def test_prime_number():
@@ -11,16 +11,14 @@ def test_non_prime_number():
     assert is_prime(4) is False
 
 
-def test_one_is_not_prime():
-    assert is_prime(1) is False
+def test_one_is_not_valid():
+    with pytest.raises(ValueError):
+        is_prime(1)
 
 
-def test_zero_is_not_prime():
-    assert is_prime(0) is False
-
-
-def test_negative_number_is_not_prime():
-    assert is_prime(-5) is False
+def test_negative_number_is_not_valid():
+    with pytest.raises(ValueError):
+        is_prime(-5)
 
 
 def test_large_prime_number():
@@ -28,10 +26,54 @@ def test_large_prime_number():
 
 
 def test_large_non_prime_number():
+    assert is_prime(81) is False
     assert is_prime(8000) is False
+
+
+expected_primes_less_than_102 = [
+    2,
+    3,
+    5,
+    7,
+    11,
+    13,
+    17,
+    19,
+    23,
+    29,
+    31,
+    37,
+    41,
+    43,
+    47,
+    53,
+    59,
+    61,
+    67,
+    71,
+    73,
+    79,
+    83,
+    89,
+    97,
+    101,
+]
 
 
 def test_prime_numbers():
     with pytest.raises(ValueError):
         prime_numbers(0)
     assert prime_numbers(10) == [2, 3, 5, 7]
+    assert prime_numbers(102) == expected_primes_less_than_102
+
+
+def test_prime_generator():
+    with pytest.raises(ValueError):
+        next(prime_generator(1))
+    assert list(prime_generator(102)) == expected_primes_less_than_102
+
+
+def test_prime_numbers_in_parallel():
+    with pytest.raises(ValueError):
+        prime_numbers_in_parallel(0)
+    assert prime_numbers_in_parallel(102) == expected_primes_less_than_102
